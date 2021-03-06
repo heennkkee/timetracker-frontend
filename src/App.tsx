b1f762ef249973890730c60783609dc94a668b0f
Henrik <henrik.aronsson.94@gmail.com>
2021-03-06 12:38:52 +0100

    Add API callback

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
 import './App.css';
 
+interface IData {
+  items: null | string[]
+}
+
 function App() {
+
+  const [ data, setData ] = useState<IData>({ items: null });
+
+  useEffect(() => {
+    const fetchAsync = async () => {
+      const resp = await fetch('https://ha-timetracker-backend.herokuapp.com').then(resp => resp.json()).then(resp => {
+        return resp;
+      });
+
+      console.log("Setting data to: ", resp.data);
+      setData({ items: resp.data });
+    }
+    
+    fetchAsync();
+
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Hola
       </header>
+      <main>
+        <p>Api responded with:</p>
+        <ul>
+          { data.items === null ? null : data.items.map((item, ix) => {
+            return <li key={ix}>{item}</li>
+          })}
+        </ul>
+      </main>
     </div>
   );
 }
